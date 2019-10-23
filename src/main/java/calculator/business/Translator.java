6378c7e081873bc4a08a6cd890501c828359155f
L4mb0 <juan.navarro@utec.edu.pe>
2019-10-23 12:04:54 -0500

    Added some headers to Translator.java

--- src/main/java/calculator/business/Translator.java
+++ src/main/java/calculator/business/Translator.java
@@ -2,6 +2,10 @@ package calculator.business;
 
 import calculator.entities.Language;
 
+import java.io.*;
+import java.net.*;
+
+
 public interface Translator {
 
     public String translate(Language from, Language to, String text);
